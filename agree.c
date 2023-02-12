ecd0e35d11aa846296bb623e1bcab1cd35a99440
naibei91 <113761851+naibei91@users.noreply.github.com>
2023-02-12 09:12:48 +0000

    Sun, Feb 12, 2023, 12:12 PM +03:00

--- agree.c
+++ agree.c
@@ -3,5 +3,18 @@
 
 int main(void)
 {
-    
+    //user to agree
+    char x = get_char("Do you agree?");
+    if (x == 'y'||x=='Y')
+    {
+        printf("Agreed\n");
+    }
+    else if (x == 'N'||x== 'n')
+    {
+        printf("Dis-agreed\n");
+    }
+    else
+    {
+        printf("Kindly type y to agree or n to disagree.\n");
+    }
 }
