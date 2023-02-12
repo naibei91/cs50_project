aedcd07aaa7b7f6ccad50dcb91d62f777e6abe44
naibei91 <113761851+naibei91@users.noreply.github.com>
2023-02-12 08:15:04 +0000

    Sun, Feb 12, 2023, 11:15 AM +03:00

--- calculato.c
+++ calculato.c
@@ -5,5 +5,5 @@ int main (void)
     //the sum of two numbers
     int a = get_int("Enter your first value:"); //first input from user
     int b=get_int("Enter your second value:"); //second input from user
-    printf("sum of %i and %i is %i \n",a,b,a+b); //output f summation
+    printf("sum of %i and %i is %i \n",a,b,a+b); //output from summation of user input
 }
