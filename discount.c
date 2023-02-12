e212a487e636908ef802c9448ab1da0737378fdb
naibei91 <113761851+naibei91@users.noreply.github.com>
2023-02-12 10:19:02 +0000

    Sun, Feb 12, 2023, 1:19 PM +03:00

--- discount.c
+++ discount.c
@@ -1,11 +1,11 @@
 #include <cs50.h>
 #include <stdio.h>
-void discount (float price)
+void discount (float price);
 
 int main (void)
 {
     float regular = get_float("What is the regular price?");
-    float sale = regular * .85;
+    float sale = discount(regular);
     printf("Current price : %.2f\n",sale);
 }
 
