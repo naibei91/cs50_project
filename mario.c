096aea9cf5c6b8eb98241a94268ab510816212a2
naibei91 <113761851+naibei91@users.noreply.github.com>
2023-02-12 10:53:56 +0000

    Sun, Feb 12, 2023, 1:53 PM +03:00

--- mario.c
+++ mario.c
@@ -3,13 +3,22 @@
 
 int main (void)
 {
-int n;
-do
-{
-    n = get_int("size?");
-}
-while (n<1);
+    int n;
 
-for
+    do
+    {
+    n = get_int("size?");
+    }
+    while (n<1);
+    //for row
+    for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)   //for column
+            {
+                printf("#");
+            }
+        //MOVE TO NEXT ROW
+        printf("\n");
+    }
 
 }
