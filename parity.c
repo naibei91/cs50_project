123aabd4a60c1d2474800686aee7f52405500f27
naibei91 <113761851+naibei91@users.noreply.github.com>
2023-02-12 09:02:17 +0000

    Sun, Feb 12, 2023, 12:02 PM +03:00

--- parity.c
+++ parity.c
@@ -1,6 +1,15 @@
 #include (stdio.h)
 #include (cs50.h)
+
 int main(void)
 {
-    
+    int a = get_int ("a : ");
+    if(a%2==0)
+    {
+        printf("even\n");
+    }
+    else
+    {
+        printf(odd\n);
+    }
 }
