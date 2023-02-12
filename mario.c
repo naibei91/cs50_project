09129f4da57809a08dae3c73907b6b10f75993c2
naibei91 <113761851+naibei91@users.noreply.github.com>
2023-02-12 10:47:28 +0000

    Sun, Feb 12, 2023, 1:47 PM +03:00

--- mario.c
+++ mario.c
@@ -6,13 +6,10 @@ int main (void)
 int n;
 do
 {
-    n = get_int("width?");
+    n = get_int("size?");
 }
 while (n<1);
 
-    for(int i=0;i<n;i++)
-    {
-        printf("?");
-    }
-    printf("\n");
+for
+
 }
