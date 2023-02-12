a99282a34e002db4d22ee5f3b9ac0edb2980c37a
naibei91 <113761851+naibei91@users.noreply.github.com>
2023-02-12 10:26:36 +0000

    Sun, Feb 12, 2023, 1:26 PM +03:00

--- discount.c
+++ discount.c
@@ -1,17 +1,17 @@
 #include <cs50.h>
 #include <stdio.h>
-float discount (float price);
+float discount (float price,percentage);
 
 int main (void)
 {
     float regular = get_float("What is the regular price?");
-    int percent =
-    float sale = discount(regular);
+    int percent_off = get_percent("What percentage do you want to take of?");
+    float sale = discount(regular,percent_off);
     printf("Current price : %.2f\n",sale);
 }
 
 
-float discount(float price)
+float discount(float price,percentage)
 {
-    return price * .85;
+    return price * (100-percentge)/100;
 }
