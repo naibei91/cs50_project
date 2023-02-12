c00b9f6fa63b456bdcd895d07e21661514f41854
naibei91 <113761851+naibei91@users.noreply.github.com>
2023-02-12 11:18:46 +0000

    Sun, Feb 12, 2023, 2:18 PM +03:00

--- calculator.c
+++ calculator.c
@@ -3,17 +3,17 @@
 
 int main (void)
 {
-    float x = get_float("Enter fitrst value:");
-    float y = get_float("Enter second value:");
-    float a = x+y;
-    float b = x*y;
-    float c = x-y;
+    int x = get_float("Enter fitrst value:");
+    int y = get_float("Enter second value:");
+   // float a = x+y;
+   // float b = x*y;
+    //float c = x-y;
     float d = x/y;
     //int e = x%y;
 
-    printf("Sum of %.2f and %f is %f\n",x,y,a);
-    printf("Multiplication of %.2f and %f is %f\n",x,y,b);
-    printf("Substraction of %f and %.f is %f\n",x,y,c);
+    //printf("Sum of %.2f and %f is %f\n",x,y,a);
+    //printf("Multiplication of %.2f and %f is %f\n",x,y,b);
+    //printf("Substraction of %f and %.f is %f\n",x,y,c);
     printf("Division of %f and %f is %.2f\n",x,y,d);
     //printf("Remainder of %f and %f is %f",x,y,e);
 }
