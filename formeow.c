337d8c8ba5b5cf805edc2874764dab948a6a1f14
naibei91 <113761851+naibei91@users.noreply.github.com>
2023-02-12 09:49:19 +0000

    Sun, Feb 12, 2023, 12:49 PM +03:00

--- formeow.c
+++ formeow.c
@@ -1,10 +1,13 @@
 # include <stdio.h>
-void meow (void)
-{
-    printf("meow\n");
-}
+void meow (void);
+
 int main(void)
     for(int i=0;i < 3;i++)
     {
         meow();
     }
+
+    void meow (void)
+{
+    printf("meow\n");
+}
