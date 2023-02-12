a4d01a0e8e8cc75d469a2cf3ef1ff624e6e449d2
naibei91 <113761851+naibei91@users.noreply.github.com>
2023-02-12 11:31:05 +0000

    Sun, Feb 12, 2023, 2:31 PM +03:00

---
+++ pennies.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+#include <cs50.h>
+
+int main (void)
+
+{
+    float amount = get_float("Enter amount in dollars: ");
+    int pennies = round(amount * 100);
+    printf ("Pennies : %i\n", pennies);
+}
