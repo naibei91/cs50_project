d135a0dd990d64b96c7ed46e8a4a5c93e2055449
naibei91 <113761851+naibei91@users.noreply.github.com>
2023-02-16 03:57:04 +0000

    Thu, Feb 16, 2023, 6:57 AM +03:00

--- bugey.c
+++ bugey.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+int multiplication(void);
 
 int main (void)
+{
+ int z = get_int("what is the size of the multiplication table? ");
+ int multiplication(int z);
+}
+
+int multiplication(void)
 {
     for (int i=1;i <=10;i++)
      {
