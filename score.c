d61f6b5631299c8b985c5a8d4982f30a362533d5
naibei91 <113761851+naibei91@users.noreply.github.com>
2023-02-17 04:24:25 +0000

    Fri, Feb 17, 2023, 7:24 AM +03:00

--- score.c
+++ score.c
@@ -12,7 +12,7 @@ int main (void)
     {
     scores[i]=get_int("Score: ");
     }
-    float aver = float average(n,scores[n]);
+    float aver = average(n,scores[n]);
     printf("Average of your scores : %f\n",aver);
 
 }
