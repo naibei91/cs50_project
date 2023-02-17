efb0c34be0b918fdeb85a88181ff5fadc55109a1
naibei91 <113761851+naibei91@users.noreply.github.com>
2023-02-17 03:55:55 +0000

    Fri, Feb 17, 2023, 6:55 AM +03:00

--- score.c
+++ score.c
@@ -3,10 +3,11 @@
 int main (void)
 
 {
-    int score1=72;
-    int score2=73;
-    int score3=33;
+    int scores[3];
+    scores[0]=72;
+    scores[1]=73;
+    scores[2]=33;
 
-    printf("Average of your scores : %f\n",(score1+score2+score3)/3.0);
+    printf("Average of your scores : %f\n",(scores[0]+scores[1]+scores[2])/3.0);
 
 }
