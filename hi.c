0dd3c89d5c8074c8a012158489aecc08b7da4e64
naibei91 <113761851+naibei91@users.noreply.github.com>
2023-02-19 02:12:00 +0000

    Sun, Feb 19, 2023, 5:12 AM +03:00

--- hi.c
+++ hi.c
@@ -4,5 +4,5 @@
 int main (void)
 {
     string s = "HI!  BYE";
- printf("%i %i %i %i %i %i %i %i %i\n",s[0],s[1],s[2],s[4],s[5],s[6],s[7],s[8],s[9]);
+ printf("%i %i %i %i\n %i %i %i %i %i",s[0],s[1],s[2],s[4],s[5],s[6],s[7],s[8],s[9]);
 }
