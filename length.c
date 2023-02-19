125344c542f9db2eed0b3f65c8dccc76657c1f10
naibei91 <113761851+naibei91@users.noreply.github.com>
2023-02-19 02:40:45 +0000

    Sun, Feb 19, 2023, 5:40 AM +03:00

--- length.c
+++ length.c
@@ -1,17 +1,26 @@
 #include <stdio.h>
 #include <cs50.h>
+int length(string name1);
+int print(int x);
 
 int main(void)
 {
     string name=get_string("what is your name?");
+    int n =length(name);
+    print(n);
+}
+int length(string name1)
+    {
     int number=0;
-
-
-    for (int i = 0;name[i]!='\0';i++)
+    for (int i = 0;name1[i]!='\0';i++)
     {
         number++;
     }
-    printf("%i\n", number);
+    return number;
+    }
 
-
-}
+int print(int x)
+    {
+    printf("%i\n", x);
+    return 0;
+    }
