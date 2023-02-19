33d7b768da29f75e512fd02e9d5cda8ba640ede8
naibei91 <113761851+naibei91@users.noreply.github.com>
2023-02-19 02:22:53 +0000

    Sun, Feb 19, 2023, 5:22 AM +03:00

--- length.c
+++ length.c
@@ -3,6 +3,14 @@
 
 int main(void)
 {
-    string s=get_string("what is your name?");
-    
+    string name=get_string("what is your name?");
+
+    int i = 0;
+
+    while(name[i]!='\0')
+    {
+        i++;
+    }
+    printf("%i\n", i);
+
 }
