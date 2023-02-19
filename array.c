2cb91e57d35143817bc7ad1628cbfbbda7df7b59
naibei91 <113761851+naibei91@users.noreply.github.com>
2023-02-19 01:23:56 +0000

    Sun, Feb 19, 2023, 4:23 AM +03:00

--- array.c
+++ array.c
@@ -3,7 +3,7 @@
 
 int number(void);
 float average(int a);
-int print(int c);
+int print(float c);
 
 int main (void)
 {
@@ -36,7 +36,7 @@ float average(int a)
     return sum/(float)a;
 }
 
-void print(int c)
+int print(float c)
 {
     printf ("The average is %f\n",c);
 }
