915789bb214c5ce15860b04ac24d12b955dfd7be
naibei91 <113761851+naibei91@users.noreply.github.com>
2023-02-19 10:46:07 +0000

    Sun, Feb 19, 2023, 1:46 PM +03:00

--- half/half.c
+++ half/half.c
@@ -18,6 +18,6 @@ int main(void)
 // TODO: Complete the function
 float half(float bill, float tax, int tip)
 {
-    float n = (bill+bill*(tax/100)+(bill*(tax/100))*((float)tip/100))/2;
+    float n = (bill + bill * (tax / 100) + (bill * (tax / 100)) * ((float)tip / 100)) / 2;
     return n;
 }
