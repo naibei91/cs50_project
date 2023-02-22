4edb74d45a96c7196065b539b7a39bdfec3056a6
naibei91 <113761851+naibei91@users.noreply.github.com>
2023-02-22 17:14:29 +0000

    Wed, Feb 22, 2023, 8:14 PM +03:00

---
+++ counting.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int get_number(void);
+void print (int a);
+
+int main (void)
+{
+   int numb = get_number();
+   print(numb);
+}
+
+int get_number(void)
+{
+    int number = get_int("To what number do you want to count?");
+    return number;
+}
+
+void print (int a)
+{
+    for(int i=1;i<=a;i++)
+    {
+        for(int j=0;j<i;j++)
+        {
+        printf("%i\n",i);
+        }
+    }
+}
