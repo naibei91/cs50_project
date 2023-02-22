669fee108c7e2ef1bac2f9f3c8507a2cd9472252
naibei91 <113761851+naibei91@users.noreply.github.com>
2023-02-22 16:35:21 +0000

    Wed, Feb 22, 2023, 7:35 PM +03:00

--- contacts.c
+++ contacts.c
@@ -10,15 +10,18 @@ contact;
 
 int main void
 {
+    string names = get_strng("Type name: ");
+    
     contact.name[0]= "Brian";
     contact.number[0]="0724846398";
 
     contact.name[1]="Amos";
     contact.number[1]="0724846398";
 
-    for(int i=0)
+    for(int i=0;i<2;i++)
     {
-
+        if(strcmp())
+        printf("");
     }
 
 
