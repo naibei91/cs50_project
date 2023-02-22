ece348b49b553ff7c178affb978228b1cc40075c
naibei91 <113761851+naibei91@users.noreply.github.com>
2023-02-22 16:51:06 +0000

    Wed, Feb 22, 2023, 7:51 PM +03:00

--- contacts.c
+++ contacts.c
@@ -13,17 +13,17 @@ int main (void)
 {
     string names = get_string("Type name: ");
 
-    contact.name[0]= "Brian";
-    contact.number[0]="0724846398";
+    contact[0].name= "Brian";
+    contact[0].number="0724846398";
 
-    contact.name[1]="Amos";
-    contact.number[1]="0724846398";
+    contact[1].name="Amos";
+    contact[1].number="0724846398";
 
     for(int i=0;i<2;i++)
     {
         if(strcmp(names,contact.name)==0)
         {
-        printf("%s:/n%s:/n",contact.name[i],contact.number[i]);
+        printf("%s:/n%s:/n",contact[i].name,contact[i].number);
         return 0;
         }
         else
