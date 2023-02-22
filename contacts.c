a1a2ca36c60ee18f7ac7dc58d4167644762393fd
naibei91 <113761851+naibei91@users.noreply.github.com>
2023-02-22 16:31:18 +0000

    Wed, Feb 22, 2023, 7:31 PM +03:00

---
+++ contacts.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <cs50.h>
+
+datatype struct
+{
+    string name;
+    string number;
+}
+contact;
+
+int main void
+{
+    contact.name[0]= "Brian";
+    contact.number[0]="0724846398";
+
+    contact.name[1]="Amos";
+    contact.number[1]="0724846398";
+
+    for(int i=0)
+    {
+
+    }
+
+
+}
