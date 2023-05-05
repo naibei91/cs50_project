8fa81bd22804b1d209484aee5e0bfd4d8b13fb1a
naibei91 <113761851+naibei91@users.noreply.github.com>
2023-05-05 17:31:17 +0000

    Fri, May 5, 2023, 8:31 PM +03:00

--- python/lesson1/grade.py
+++ python/lesson1/grade.py
@@ -15,7 +15,7 @@ else:
 
 if 100>=score>=90:
     print(f"A : {score}%")
-elif score >=80 and score<90:
+elif 80>=score<90:
     print(f"B : {score}%")
 elif score >=70 and score<80:
     print(f"C : {score}%")
@@ -23,3 +23,16 @@ elif score >=60 and score<70:
     print(f"D : {score}%")
 else:
     print(f"E : {score}%")
+
+#also
+if score>=90:
+    print(f"A : {score}%")
+elif score>=80:
+    print(f"B : {score}%")
+elif score >=70:
+    print(f"C : {score}%")
+elif score >=60:
+    print(f"D : {score}%")
+else:
+    print(f"E : {score}%")
+
