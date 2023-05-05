03f3fa544b5177ea9b69fd4bd2a2138687ae6721
naibei91 <113761851+naibei91@users.noreply.github.com>
2023-05-05 11:22:18 +0000

    Fri, May 5, 2023, 2:22 PM +03:00

--- python/lesson0/hello1.py
+++ python/lesson0/hello1.py
@@ -1,13 +1,14 @@
 def main():
     name = input("what is your name ?").title()
     hello1(name)
+    hello
 def hello ():
     print("hello",end = " ")
-    
+
 def hello1(to = "world"):    #just
     print("hello "+ to)
 
 
-
+main()
 
 
