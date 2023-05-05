da99e16a1c15a37f8ad00cd2bbf11ad65a671682
naibei91 <113761851+naibei91@users.noreply.github.com>
2023-05-05 17:27:30 +0000

    Fri, May 5, 2023, 8:27 PM +03:00

--- python/lesson1/grade.py
+++ python/lesson1/grade.py
@@ -1,11 +1,25 @@
-score = (int("What is the score of the student"))
+score = int(input("What is the score of the student : ").replace("%",""))
 if score >=90 and score <=100:
-    print(f"A : {x}")
+    print(f"A : {score}%")
 elif score >=80 and score<90:
-    print(f"B : {x}")
+    print(f"B : {score}%")
 elif score >=70 and score<80:
-    print(f"C : {x}")
+    print(f"C : {score}%")
 elif score >=60 and score<70:
-    print(f"D : {x}")
-else
-    print(f"E : {x}")
+    print(f"D : {score}%")
+else:
+    print(f"E : {score}%")
+
+
+#also...
+
+if 100>=score<=90:
+    print(f"A : {score}%")
+elif score >=80 and score<90:
+    print(f"B : {score}%")
+elif score >=70 and score<80:
+    print(f"C : {score}%")
+elif score >=60 and score<70:
+    print(f"D : {score}%")
+else:
+    print(f"E : {score}%")
