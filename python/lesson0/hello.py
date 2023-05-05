4d6f93219e1fe78edfb1a6eb475306e1aeeaf9f5
naibei91 <113761851+naibei91@users.noreply.github.com>
2023-05-05 09:03:40 +0000

    Fri, May 5, 2023, 12:03 PM +03:00

--- python/lesson0/hello.py
+++ python/lesson0/hello.py
@@ -37,7 +37,7 @@ print(f"Hello,\"{name}\"{name1}",sep=' ')
 
 """
 #using it in one line of code
-name2 = input("What are your names ?").strip("b").title()
+name2 = input("What are your names ?").title().strip("b")
 print(f"Hi,{name2}")
 
 #split users name into first name and last name
