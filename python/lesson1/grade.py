85a3644a139ffaf736e58e5d396d52131e20da47
naibei91 <113761851+naibei91@users.noreply.github.com>
2023-05-05 17:34:52 +0000

    Fri, May 5, 2023, 8:34 PM +03:00

--- python/lesson1/grade.py
+++ python/lesson1/grade.py
@@ -8,7 +8,7 @@ elif score >=70 and score<80:
 elif score >=60 and score<70:
     print(f"D : {score}%")
 else:
-    print(f"E : {score}%")
+    print(f"F : {score}%")
 
 
 #also...
@@ -17,12 +17,12 @@ if 100>=score>=90:
     print(f"A : {score}%")
 elif 80>=score<90:
     print(f"B : {score}%")
-elif score >=70 and score<80:
+elif 70>=score<80 :
     print(f"C : {score}%")
-elif score >=60 and score<70:
+elif 60>=score<70 :
     print(f"D : {score}%")
 else:
-    print(f"E : {score}%")
+    print(f"F : {score}%")
 
 #also
 if score>=90:
@@ -34,5 +34,5 @@ elif score >=70:
 elif score >=60:
     print(f"D : {score}%")
 else:
-    print(f"E : {score}%")
+    print(f"F : {score}%")
 
