b4b477b03e7667a8693478ec4876a70af0a2ff71
naibei91 <113761851+naibei91@users.noreply.github.com>
2023-05-05 12:54:41 +0000

    Fri, May 5, 2023, 3:54 PM +03:00

--- faces/faces.py
+++ faces/faces.py
@@ -1,5 +1,5 @@
 def main():
-    face = input("How are you feeling today : ").replace(":)","u+263A")
+    face = input("How are you feeling today : ").replace(":)",).replace(":(",🙁)
     convert(face)
 def convert(face1):
     print(f"HELLO {face1}")
