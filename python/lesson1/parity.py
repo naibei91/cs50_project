c95971764d6a9778d7221b704f113933e307c3dd
naibei91 <113761851+naibei91@users.noreply.github.com>
2023-05-05 18:18:56 +0000

    Fri, May 5, 2023, 9:18 PM +03:00

--- python/lesson1/parity.py
+++ python/lesson1/parity.py
@@ -1,10 +1,13 @@
 
-def main(y):
-
-if x % 2 ==0:
-    print(f"{x} is even")
-else:
-    print(f"{x} is odd")
+def main():
+    even(value())
 
 def value():
-    details = ()
+    details = int(input("Enter an even number : "))
+    return details
+
+def even (x):
+    if x % 2 ==0:
+        print(f"{x} is even")
+    else:
+        print(f"{x} is odd")
