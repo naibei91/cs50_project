2ae47748dcbb5410551029e163dae5083dbb3360
naibei91 <113761851+naibei91@users.noreply.github.com>
2023-05-05 09:26:16 +0000

    Fri, May 5, 2023, 12:26 PM +03:00

--- python/lesson0/calculator.py
+++ python/lesson0/calculator.py
@@ -10,3 +10,10 @@ z = int(x)+int(y)
 print(f"sum of first value and second value is {z}")
 
 #using float
+x = float(input("Enter first value:"))
+y = float(input("Enter second value:"))
+
+print(x+y)
+
+z = x.2+y.2
+print(f"sum of first value and second value is {z}")
