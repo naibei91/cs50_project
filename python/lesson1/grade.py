674ac3b7f489707240adc9512ae12201819e7641
naibei91 <113761851+naibei91@users.noreply.github.com>
2023-05-05 17:21:34 +0000

    Fri, May 5, 2023, 8:21 PM +03:00

---
+++ python/lesson1/grade.py
@@ -0,0 +1,11 @@
+score = (int("What is the score of the student"))
+if score >=90 and score <=100:
+    print(f"A : {x}")
+elif score >=80 and score<90:
+    print(f"B : {x}")
+elif score >=70 and score<80:
+    print(f"C : {x}")
+elif score >=60 and score<70:
+    print(f"D : {x}")
+else
+    print(f"E : {x}")
