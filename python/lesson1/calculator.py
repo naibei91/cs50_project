25953ec9cbf45a13c2f78afda68f21a93e411247
naibei91 <113761851+naibei91@users.noreply.github.com>
2023-05-13 07:22:02 +0000

    Sat, May 13, 2023, 10:22 AM +03:00

--- python/lesson1/calculator.py
+++ python/lesson1/calculator.py
@@ -5,16 +5,16 @@ def display():
     x = (input("input first value : "))
     y = x.rfind()
     z = int(input("Choose 1 for sum 2 for differences 3 for multiplication 4 for remainder 5 for "))
-    if(z == 1):
+    if(y == +):
         sum(x,y)
-    elif(z == 2):
+    elif(y == -):
         diff(x,y)
-    elif(z == 3):
+    elif( == *):
         mult(x,y)
-    elif(z == 4):
+    elif(y == %):
         rem(x,y)
     else:
-        return 0
+        return z
 
 def sum(a,b):
     print(f"The sum is of {a} and {b}: {a+b}")
