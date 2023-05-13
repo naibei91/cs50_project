9723a7609c5463483e5aaa5b8e56573578b947b2
naibei91 <113761851+naibei91@users.noreply.github.com>
2023-05-13 06:15:52 +0000

    Sat, May 13, 2023, 9:15 AM +03:00

--- python/lesson1/training.py
+++ python/lesson1/training.py
@@ -2,10 +2,30 @@ def main():
     display()
 
 def display():
-    x = double(input("input first value : "))
-    y = double("input next value : ")
-    z = int(input(""))
-    sum()
+    x = float(input("input first value : "))
+    y = float(input("input next value : "))
+    z = int(input("Choose 1 for sum 2 for differences 3 for multiplication 4 for remainder 5 for "))
+    if(z == 1):
+        sum(x,y)
+    elif(z == 2):
+        diff(x,y)
+    elif(z == 3):
+        mult(x,y)
+    elif(z == 4):
+        rem(x,y)
+    else:
+        return 0
 
-def sum():
-    return a+b
+def sum(a,b):
+    print(f"The sum is of {a} and {b}: {a+b}")
+
+def diff(c,d):
+    print(f"The difference is {c} and {d} : {c-d}")
+
+def mult(e,f):
+    print(f"The sum is of {e} and {f}: {e*f}")
+
+def rem(g,h):
+    print(f"The remainde is of {g} dividing {h}: {g%h}")
+
+main()
