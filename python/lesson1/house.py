ec343f1afbed85fa95fd9dac9d50be5c80954db6
naibei91 <113761851+naibei91@users.noreply.github.com>
2023-05-14 07:18:07 +0000

    Sun, May 14, 2023, 10:18 AM +03:00

--- python/lesson1/house.py
+++ python/lesson1/house.py
@@ -1,9 +1,9 @@
 name = input("Search users to know thier department : ").title().strip(" ")
-if name == "John" or "Jane" or "Peter":
+if name == "John" or name == "Jane" or name == "Peter":
     print("Sales")
-elif name == "Onyango" or "Jane" or "Sarah":
+elif name == "Onyango" or name == "Jane" or name == "Sarah":
     print("Purchases")
-elif name == "Mwangi" or "Waweru" or "Kimani":
+elif name == "Mwangi" or name == "Waweru" or name == "Kimani":
     print("Finance")
 else:
     print("Who?")
