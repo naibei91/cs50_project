960fa09cd48f368102351629af69938ce3a4137b
naibei91 <113761851+naibei91@users.noreply.github.com>
2023-05-14 17:09:45 +0000

    Sun, May 14, 2023, 8:09 PM +03:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,11 +1,10 @@
 def main():
-
+    meal convert(y)
 
 
 def convert(time):
     ...
 
-x,y = input("What is the time ?").split(".")
-print(x)
+x,y = input("What is the time ?").split(":")
 if x == "7" or x == "8" or x == "12" x == "13" x == "18" x =="19":
     main()
