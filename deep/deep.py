f5c381ba4f284e708b6f60935ba329c837ca170b
naibei91 <113761851+naibei91@users.noreply.github.com>
2023-05-14 07:53:55 +0000

    Sun, May 14, 2023, 10:53 AM +03:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,6 +1,6 @@
-x = input("What is the answer to the Great Question of Life, the Universe and Everything?")
+x = input("What is the answer to the Great Question of Life, the Universe and Everything?").title()
 match x:
-    case "42" | "forty-two" | "forty two":
+    case "42" | "Forty-Two" | "Forty Two":
         print("Yes")
     case _:
         print("No")
