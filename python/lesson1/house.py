92add65c63ea7d0c02a752a01cdee485ef2e5fba
naibei91 <113761851+naibei91@users.noreply.github.com>
2023-05-14 07:07:32 +0000

    Sun, May 14, 2023, 10:07 AM +03:00

--- python/lesson1/house.py
+++ python/lesson1/house.py
@@ -1,1 +1,7 @@
-name = input("").title()
+name = input("Search users by department : ").title()
+if name == "Sales":
+    print("1.John\n2.Jane\n3.Peter")
+elif name == "Purchases":
+    print("1.Onyango\n2.Jane\n3.Sarah")
+else:
+    print("Which department ?")
