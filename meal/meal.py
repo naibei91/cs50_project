21ae74743743212cf98357db90ddd2d108d978e8
naibei91 <113761851+naibei91@users.noreply.github.com>
2023-05-14 17:40:45 +0000

    Sun, May 14, 2023, 8:40 PM +03:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,5 +1,12 @@
 def main():
     meal = convert(b)
+    if 7.0<=meal<=8.0:
+        print("breakfast time")
+    elif 12.0<=meal<=13.0:
+        print("lunch time")
+    elif 18.0<=meal<=19.0:
+        print("dinner time")
+
 
 def convert(time):
     c = round(a+(time/60),2)
