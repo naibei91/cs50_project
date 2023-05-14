e4c18d261e101083ee5da85abcc95124de837c75
naibei91 <113761851+naibei91@users.noreply.github.com>
2023-05-14 15:46:48 +0000

    Sun, May 14, 2023, 6:46 PM +03:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -1,11 +1,12 @@
 def main():
     x = input("FIle name : ").title()
-    y = x+" "
-    z1 = int(y.index("."))
-    z2 = int(y.rindex("."))
-    a = y[z1+1]+y[z1+2]+y[z1+3]+y[z1+4].strip()
-    if z1 == z2 :
-        onyesha(a)
+    if x.find(".") == True:
+        y = x+" "
+        z1 = int(y.index("."))
+        z2 = int(y.rindex("."))
+        a = y[z1+1]+y[z1+2]+y[z1+3]+y[z1+4].strip()
+        if z1 == z2 :
+            onyesha(a)
     else:
        print("application/octet-stream")
 
