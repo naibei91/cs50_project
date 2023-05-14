9740cbfaaadf3ee8ead5db098b57496ad9a74245
naibei91 <113761851+naibei91@users.noreply.github.com>
2023-05-14 08:52:29 +0000

    Sun, May 14, 2023, 11:52 AM +03:00

--- bank/bank.py
+++ bank/bank.py
@@ -1,6 +1,6 @@
 greetings = input("Greating : ").title().strip()
 
-if greetings.find("Hello") == "Hello":
+if greetings.find("Hello") != -1:
     print("$0")
 elif greetings[0] == "H":
     print("$20")
