493b523f11b8696151dd9a22220c9049da3a52ac
naibei91 <113761851+naibei91@users.noreply.github.com>
2023-05-14 15:29:41 +0000

    Sun, May 14, 2023, 6:29 PM +03:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -5,23 +5,25 @@ def main():
     z2 = int(y.rindex("."))
     a = y[z1+1]+y[z1+2]+y[z1+3]+y[z1+4].strip()
     if z1 == z2 :
-    print(a)
-    else:
-    print("application/octet-stream")
-def print(b):
-    match a:
-            case "gif" :
-                print("image/gif")
+        onyesha(a)
+    #else:
+     #   print("application/octet-stream")
 
-            case "jpeg" | "jpg":
-                print("image/jpeg")
-            case "png":
-                print("image/png")
-            case "pdf":
-                print("application/pdf")
-            case "text":
-                print("text/plain")
-            case "zip":
-                print("application/zip")
-            case _:
-                print("application/octet-stream")
+def onyesha(b):
+    match b:
+        case "gif" :
+            print("image/gif")
+        case "jpeg" | "jpg":
+            print("image/jpeg")
+        case "png":
+            print("image/png")
+        case "pdf":
+            print("application/pdf")
+        case "text":
+            print("text/plain")
+        case "zip":
+            print("application/zip")
+        case _:
+            print("application/octet-stream")
+
+main()
