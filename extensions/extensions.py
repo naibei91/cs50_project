a29aad520daf7fcfc3fdf878053cad4661857502
naibei91 <113761851+naibei91@users.noreply.github.com>
2023-05-14 14:20:06 +0000

    Sun, May 14, 2023, 5:20 PM +03:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -4,18 +4,18 @@ z = y.find(".")
 a = y[z+1]+y[z+2]+y[z+3]+y[z+4].strip()
 match a:
     case "gif":
-        print(f"{x}").replace(".gif","/gif")
+        print(f"{x}".replace(".gif","/gif"))
     case "jpg":
-        print(f"{x}").replace(".jpg","/jpg")
+        print(f"{x}".replace(".jpg","/jpg"))
     case "jpeg":
-        print(f"{x}").replace(".jpeg","/jpeg")
+        print(f"{x}".replace(".jpeg","/jpeg"))
     case "png":
-        print(f"{x}").replace(".png","/png")
+        print(f"{x}".replace(".png","/png"))
     case "pdf":
-        print(f"{x}").replace(".pdf","/pdf")
+        print(f"{x}".replace(".pdf","/pdf"))
     case "text":
-        print(f"{x}").replace(".text","/text")
+        print(f"{x}".replace(".text","/text"))
     case "zip":
-        print(f"{x}").replace(".zip","/zip")
+        print(f"{x}".replace(".zip","/zip"))
 
 
