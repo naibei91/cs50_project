12f8f185745f160c65f5db6ec144d5e65e496986
naibei91 <113761851+naibei91@users.noreply.github.com>
2023-05-14 17:17:32 +0000

    Sun, May 14, 2023, 8:17 PM +03:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,10 +1,10 @@
 def main():
-    meal convert(y)
-    if 7.0<=meal<=8.0
+    meal = convert(y)
+    if 7.0<=meal<=8.0:
         print("breakfast time")
-    if 7.0<=meal<=8.0
+    elif 12.0<=meal<=13.0:
         print("lunch time")
-    if 7.0<=meal<=8.0
+    elif 18.0<=meal<=19.0:
         print("dinner time")
 
 
@@ -12,5 +12,5 @@ def convert(time):
     return round(x+y/60,2)
 
 x,y = input("What is the time ?").split(":")
-if x == "7" or x == "8" or x == "12" x == "13" x == "18" x =="19":
+if x == "7" or x == "8" or x == "12" or x == "13" or x == "18" or x =="19":
     main()
