e765faaa526dd052ab4880d4756572bc0e71555c
naibei91 <113761851+naibei91@users.noreply.github.com>
2023-05-14 14:17:20 +0000

    Sun, May 14, 2023, 5:17 PM +03:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -1,20 +1,21 @@
 x = input("FIle name : ").strip()
 y = x+" "
 z = y.find(".")
-a = y[z+1]+y[z+2]+y[z+3]+y[z+4]
+a = y[z+1]+y[z+2]+y[z+3]+y[z+4].strip()
 match a:
-    case gif:
+    case "gif":
         print(f"{x}").replace(".gif","/gif")
-    case jpg:
+    case "jpg":
         print(f"{x}").replace(".jpg","/jpg")
-    case jpeg:
+    case "jpeg":
         print(f"{x}").replace(".jpeg","/jpeg")
-    case png:
-        print(f"{x}").replace(".gif","/gif")
-    case pdf:
-        print(f"{x}").replace(".gif","/gif")
-    case text:
-        print(f"{x}").replace(".gif","/gif")
-    case zip:
-        print(f"{x}").replace(".gif","/gif")
+    case "png":
+        print(f"{x}").replace(".png","/png")
+    case "pdf":
+        print(f"{x}").replace(".pdf","/pdf")
+    case "text":
+        print(f"{x}").replace(".text","/text")
+    case "zip":
+        print(f"{x}").replace(".zip","/zip")
+
 
