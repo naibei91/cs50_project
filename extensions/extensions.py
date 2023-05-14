cf3a39f2c05451ac98967da89ddf27ba7f2d2130
naibei91 <113761851+naibei91@users.noreply.github.com>
2023-05-14 14:12:41 +0000

    Sun, May 14, 2023, 5:12 PM +03:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -4,5 +4,27 @@ z = y.find(".")
 a = y[z+1]+y[z+2]+y[z+3]+y[z+4]
 match a:
     case gif:
-        print(f"{x}").replace(".img","/gif")"""
+        print(f"{x}").replace(".gif","/gif")
+    case jpg:
+        print(f"{x}").replace(".jpg","/jpg")
+    case jpeg:
+        print(f"{x}").replace(".jpeg","/jpeg")
+    case gif:
+        print(f"{x}").replace(".gif","/gif")
+    case gif:
+        print(f"{x}").replace(".gif","/gif")
+    case gif:
+        print(f"{x}").replace(".gif","/gif")
+    case gif:
+        print(f"{x}").replace(".gif","/gif")
 
+
+
+
+gif
+.jpg
+.jpeg
+.png
+.pdf
+.txt
+.zip
