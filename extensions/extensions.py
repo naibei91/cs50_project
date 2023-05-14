335b0f11a563f489bfa205f42782bac389705aaf
naibei91 <113761851+naibei91@users.noreply.github.com>
2023-05-14 14:08:09 +0000

    Sun, May 14, 2023, 5:08 PM +03:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -1,7 +1,7 @@
 x = input("FIle name : ").strip()
-
-y = x.find(".")
-a = x[y+1]+x[y+2]+x[y+3]+x[y+4]
+y = x+" "
+z = y.find(".")
+a = x[z+1]+x[z+2]+x[z+3]+x[z+4]
 print(a)
 """match a:
     case img:
