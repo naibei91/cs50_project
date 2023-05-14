5a04eca2b563345918f962bec415b09d82e893dd
naibei91 <113761851+naibei91@users.noreply.github.com>
2023-05-14 14:36:10 +0000

    Sun, May 14, 2023, 5:36 PM +03:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -1,21 +1,24 @@
 x = input("FIle name : ").strip().title()
 y = x+" "
 z = y.find(".")
-a = y[z+1]+y[z+2]+y[z+3]+y[z+4].strip()
-match a:
-    case "gif" :
-        print("image/gif")
-    case "jpeg" | "jpg":
-        print("image/jpeg")
-    case "png":
-        print("image/png")
-    case "pdf":
-        print("application/pdf")
-    case "text":
-        print("text/plain")
-    case "zip":
-        print("application/zip")
-    case _:
-        print("application/octet-stream")
+if z ==
+    a = y[z+1]+y[z+2]+y[z+3]+y[z+4].strip()
+        match a:
+            case "gif" :
+                print("image/gif")
+            case "jpeg" | "jpg":
+                print("image/jpeg")
+            case "png":
+                print("image/png")
+            case "pdf":
+                print("application/pdf")
+            case "text":
+                print("text/plain")
+            case "zip":
+                print("application/zip")
+            case _:
+                print("application/octet-stream")
 
+elif:
+    print("application/octet-stream")
 
