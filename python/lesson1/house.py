831ee85530219107637a929f089b40631e283207
naibei91 <113761851+naibei91@users.noreply.github.com>
2023-05-14 07:10:33 +0000

    Sun, May 14, 2023, 10:10 AM +03:00

--- python/lesson1/house.py
+++ python/lesson1/house.py
@@ -3,5 +3,7 @@ if name == "Sales":
     print("1.John\n2.Jane\n3.Peter")
 elif name == "Purchases":
     print("1.Onyango\n2.Jane\n3.Sarah")
+elif name == "Finance":
+    print("1.Mwangi\n2.Waweru\n3.Kimani")
 else:
     print("Which department ?")
