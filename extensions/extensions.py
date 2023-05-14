d3034a57ae1819dcf8a1fa4d3d917ec173a3bf78
naibei91 <113761851+naibei91@users.noreply.github.com>
2023-05-14 14:57:15 +0000

    Sun, May 14, 2023, 5:57 PM +03:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -1,8 +1,9 @@
 x = input("FIle name : ").strip().title()
 y = x+" "
-z = y.find(".")
+z1 = y.find(".")
+z2 = y.rindex(".")
 a = y[z+1]+y[z+2]+y[z+3]+y[z+4].strip()
-if z == 1:
+if z1 == z2:
        match a:
             case "gif" :
                 print("image/gif")
